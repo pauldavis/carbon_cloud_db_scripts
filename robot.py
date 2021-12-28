a29b02658653790824758ae91860e77febb63dc7
pauldavis <paul@techquity.ai>
2021-12-28 20:18:22 +0100

    start of new robot simulator at robot.py

--- robot.py
+++ robot.py
@@ -2,28 +2,23 @@
 import requests
 
 # defining the api-endpoint
-API_ENDPOINT = "http://pastebin.com/api/api_post.php"
+API_ENDPOINT = "http://localhost:3000/robot_statuses"
 
-# your API key here
-API_KEY = "XXXXXXXXXXXXXXXXX"
-
-# your source code here
-source_code = '''
-print("Hello, world!")
-a = 1
-b = 2
-print(a + b)
-'''
-
-# data to be sent to api
-data = {'api_dev_key':API_KEY,
-        'api_option':'paste',
-        'api_paste_code':source_code,
-        'api_paste_format':'python'}
+status_data  = '''{"robot_id": "3f54b65a-675b-11ec-a68d-0242ac110002",
+                 "status_time": "2021-12-27T22:30:09.176+00:00",
+                "status_location": {
+                        "type": "Point",
+                        "coordinates": [
+                                50.7373,
+                                3.075
+                                ]
+                                }
+                }
+                '''
 
 # sending post request and saving response as response object
-r = requests.post(url = API_ENDPOINT, data = data)
+r = requests.post(url = API_ENDPOINT, data = status_data)
 
 # extracting response text
 pastebin_url = r.text
-print("The pastebin URL is:%s"%pastebin_url)
+print("The robot response is:%s"%pastebin_url)
